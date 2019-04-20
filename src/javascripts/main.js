e8331976bd03140011c78f6f56932778c74c6153
Andrew Tiller <tiller.andrew@gmail.com>
2019-04-20 11:41:10 -0500

    added sleep

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,12 +1,14 @@
 import eat from './components/eat/eat';
 import play from './components/play/play';
 import fight from './components/fight/fight';
+import sleep from './components/sleep/sleep';
 import '../styles/main.scss';
 
 const init = () => {
   eat.printEat();
   play.printPlay();
   fight.printFight();
+  sleep.printSleep();
 };
 
 init();
